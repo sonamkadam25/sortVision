b96409d6490982039532150824943398190dfec0
Sonam Kadam <125948681+sonamkadam25@users.noreply.github.com>
2024-06-13 15:16:39 +0530

    Update heap_sort.js

--- heap_sort.js
+++ heap_sort.js
@@ -6,7 +6,7 @@ function Heap()
     document.getElementById("Time_Worst").innerText="O(N log N)";
     document.getElementById("Time_Average").innerText="Θ(N log N)";
     document.getElementById("Time_Best").innerText="Ω(N log N)";
-
+ 
     //Setting Space complexity
     document.getElementById("Space_Worst").innerText="O(1)";
 
