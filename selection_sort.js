bb6cdbf40943a5d53f2c1a3f2b63691a1e998cad
Sonam Kadam <125948681+sonamkadam25@users.noreply.github.com>
2024-06-13 15:16:21 +0530

    Update selection_sort.js

--- selection_sort.js
+++ selection_sort.js
@@ -4,7 +4,7 @@ function Selection_sort()
     document.getElementById("Time_Worst").innerText="O(N^2)";
     document.getElementById("Time_Average").innerText="Θ(N^2)";
     document.getElementById("Time_Best").innerText="Ω(N^2)";
-
+ 
     //Setting Space complexity
     document.getElementById("Space_Worst").innerText="O(1)";
 
